2b884f6110b1dda47b4f1ea9769e172439e7b115
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-12-07 10:20:46 +0800

    Initial commit

---
+++ src/main/java/cn/com/xuxiaowei/WebServicesApplication.java
@@ -0,0 +1,18 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ */
+@SpringBootApplication
+public class WebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesApplication.class, args);
+    }
+
+}
