33bc7758911dd647b4190992a6adec422ee0fc87
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-12-12 09:20:56 +0800

    打 war 包

---
+++ src/main/java/cn/com/xuxiaowei/configuration/ServletInitializer.java
@@ -0,0 +1,20 @@
+//package cn.com.xuxiaowei.configuration;
+//
+//import cn.com.xuxiaowei.WebServicesApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+///**
+// * 仅在 打成 war 发布到 Tomcat 上时启用
+// *
+// * @author xuxiaowei
+// * @since 0.0.1
+// */
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//
+//        return builder.sources(WebServicesApplication.class);
+//    }
+//}
