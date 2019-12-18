03fe06a014aca59882e90b6d9202d00e3493c2e4
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2019-12-18 11:32:29 +0800

    清理

--- src/main/java/cn/com/xuxiaowei/configuration/MybatisPlusConfiguration.java
+++ src/main/java/cn/com/xuxiaowei/configuration/MybatisPlusConfiguration.java
@@ -28,28 +28,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MybatisPlusConfiguration {
 
     /**
-     *
-     * @see <a href="https://mp.baomidou.com/guide/performance-analysis-plugin.html#%E6%80%A7%E8%83%BD%E5%88%86%E6%9E%90%E6%8F%92%E4%BB%B6">性能分析插件</a>
-     *
-     * SQL执行效率插件
-     * <p>
-     * 设置 dev-sql 环境开启
-     * <p>
-     * Profile({"dev-sql"})（需要在配置文件中激活）
-     */
-//    @Bean
-//    @Profile({"dev-sql"})
-//    PerformanceInterceptor performanceInterceptor() {
-//
-//        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-//
-//        /* SQL 是否格式化 */
-//        performanceInterceptor.setFormat(true);
-//
-//        return performanceInterceptor;
-//    }
-
-    /**
      * 分页插件
      */
     @Bean
